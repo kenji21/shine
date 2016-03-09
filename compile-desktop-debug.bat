326472d0ac5a5e0f2a090430e9dd1003247d356a
kenji21 <r.bergoin@openium.fr>
2016-03-09 22:03:27 +0100

    Updates windows compile scripts for Qt5

--- compile-desktop-debug.bat
+++ compile-desktop-debug.bat
@@ -1,19 +1,9 @@
-mkdir build-desktop-qjson-win-debug
-mkdir qjson-install-win-debug
-
-cd build-desktop-qjson-win-debug
-
-cmake.exe -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../qjson-install-win-debug ../qjson
-
-mingw32-make.exe
-mingw32-make.exe install
-
-cd ..
-
 mkdir build-desktop-win-debug
 cd build-desktop-win-debug
 
-cmake.exe -G "MinGW Makefiles" .. -DDESKTOP_BUILD=1 -DCMAKE_BUILD_TYPE=Debug
+set PATH=C:\Program Files\CMake\bin;C:\Qt\Qt5.5.1\Tools\mingw492_32\bin;C:\Qt\Qt5.5.1\bin;
+
+cmake.exe -G "MinGW Makefiles" .. -DDESKTOP_BUILD=1 -DCMAKE_PREFIX_PATH=C:\Qt\Qt5.5.1\5.5\mingw492_32\lib\cmake -DWITH_QT5=1 -DCMAKE_BUILD_TYPE=Debug
 
 mingw32-make.exe
 
--- compile-desktop.bat
+++ compile-desktop.bat
@@ -1,23 +1,10 @@
-mkdir build-desktop-qjson-win
-mkdir qjson-install-win
-
-cd build-desktop-qjson-win
-
-cmake.exe -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=../qjson-install-win ../qjson
-
-mingw32-make.exe
-mingw32-make.exe install
-
-cd ..
-
 mkdir build-desktop-win
 cd build-desktop-win
 
-cmake.exe -G "MinGW Makefiles" .. -DDESKTOP_BUILD=1
-:: -DCMAKE_PREFIX_PATH=C:\Qt\Qt5.5.1\5.5\mingw492_32\bin -DWITH_QT5=1
+set PATH=C:\Program Files\CMake\bin;C:\Qt\Qt5.5.1\Tools\mingw492_32\bin;C:\Qt\Qt5.5.1\bin;
+
+cmake.exe -G "MinGW Makefiles" .. -DDESKTOP_BUILD=1 -DCMAKE_PREFIX_PATH=C:\Qt\Qt5.5.1\5.5\mingw492_32\lib\cmake -DWITH_QT5=1
 
 mingw32-make.exe
 
 cd ..
-
-pause
